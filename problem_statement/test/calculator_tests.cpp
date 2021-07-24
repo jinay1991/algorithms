cd8eba60ddae80a089a29317815ad64666f1411f
Jinay Patel <jinay1991@gmail.com>
2021-07-24 10:28:53 +0000

    Update  :

--- problem_statement/test/calculator_tests.cpp
+++ problem_statement/test/calculator_tests.cpp
@@ -17,34 +17,41 @@ namespace
 struct TestStringParam
 {
     // Given
-    std::string input;
+    std::string expression;
 
     // Then
     double result;
 };
 
-class CalculatorFixture_WithStringInput : public ::testing::TestWithParam<TestStringParam>
+class CalculatorFixture_WithExpression : public ::testing::TestWithParam<TestStringParam>
 {
 };
+// clang-format off
+INSTANTIATE_TEST_SUITE_P(
+    Calculator,
+    CalculatorFixture_WithExpression,
+    ::testing::Values(
+        //              expression              , (expected) result
+        TestStringParam{"1+2"                   ,       3.0       }, // (0)
+        TestStringParam{"10*20"                 ,       200.0     }, // (1)
+        TestStringParam{"5-1/5+16+4*3"          ,       32.79     }, // (2)
+        TestStringParam{"10 + 2 * 6"            ,       22.0      }, // (3)
+        TestStringParam{"100 * ( 2 + 12 ) / 14" ,       100.0     }, // (4)
+        TestStringParam{"100 * ( 2 + 12 )"      ,       1400.0    }, // (5)
+        TestStringParam{"100 * 2 + 12"          ,       212.0     }  // (6)
+));
+// clang-format on
 
-INSTANTIATE_TEST_SUITE_P(Calculator,
-                         CalculatorFixture_WithStringInput,
-                         ::testing::Values(
-                             //
-                             TestStringParam{"1+2", 3.0},
-                             TestStringParam{"10*20", 200.0},
-                             TestStringParam{"5-1/5+16+4*3", 0.0}));
-
-TEST_P(CalculatorFixture_WithStringInput, GivenTypicalString_ExpectCalculatedResult)
+TEST_P(CalculatorFixture_WithExpression, GivenTypicalString_ExpectCalculatedResult)
 {
     // Given
     const auto param = GetParam();
 
     // When
-    const auto result = calculate(param.input);
+    const auto result = calculate(param.expression);
 
     // Then
-    EXPECT_DOUBLE_EQ(result, param.result);
+    EXPECT_NEAR(result, param.result, 0.01);
 }
 }  // namespace
 }  // namespace problem_statement
