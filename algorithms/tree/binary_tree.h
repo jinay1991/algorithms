1e8840b88993d83403db5a435c3facd7631bb7eb
Jinay Patel <jinay1991@gmail.com>
2021-07-25 16:35:28 +0000

    Add BinaryTree (Array)

---
+++ algorithms/tree/binary_tree.h
@@ -0,0 +1,199 @@
+///
+/// @file
+/// @copyright Copyright (C) 2021. MIT License.
+///
+#ifndef ALGORITHMS_TREE_BINARY_TREE_H
+#define ALGORITHMS_TREE_BINARY_TREE_H
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <ostream>
+
+namespace tree
+{
+
+template <typename T, std::size_t kMaxNumberOfNodes, bool kIsBinarySearchTree = true>
+class Tree
+{
+  public:
+    using value_type = T;
+    using index_type = std::size_t;
+    using size_type = std::size_t;
+    using container = std::array<value_type, kMaxNumberOfNodes>;
+
+    constexpr Tree() : invalid_value_{}, size_{0UL}, buffer_{} {}
+    constexpr explicit Tree(const value_type& invalid_value) : invalid_value_{invalid_value}, size_{0UL}, buffer_{} {}
+
+    constexpr void Insert(const value_type& value)
+    {
+        if (!IsFull())
+        {
+            if (kIsBinarySearchTree)
+            {
+                InsertBST(GetRootIndex(), value);
+            }
+            else
+            {
+                InsertBT(GetRootIndex(), value);
+            }
+        }
+    }
+
+    constexpr void Remove(const value_type& value)
+    {
+        if (!IsEmpty())
+        {
+            const auto it = std::find(buffer_.begin(), buffer_.end(), value);
+            if (it != buffer_.end())
+            {
+                const index_type remove_index = std::distance(buffer_.begin(), it);
+                SetNodeValue(remove_index, GetInvalidValue());
+                size_--;
+            }
+        }
+    }
+
+    constexpr size_type GetSize() const { return size_; }
+    constexpr size_type GetCapacity() const { return buffer_.size(); }
+
+    constexpr container GetInOrderTraversal() const { return InOrder(GetRootIndex()); }
+    constexpr container GetPostOrderTraversal() const { return PostOrder(GetRootIndex()); }
+    constexpr container GetPreOrderTraversal() const { return PreOrder(GetRootIndex()); }
+    constexpr size_type GetMaxDepth() const { return MaxDepth(GetRootIndex()); }
+    constexpr size_type GetMinValue() const { return MinValue(GetRootIndex()); }
+    constexpr size_type GetMaxValue() const { return MaxValue(GetRootIndex()); }
+    constexpr bool IsBinarySearchTree() const { return IsBinarySearchTree(GetRootIndex()); }
+
+  private:
+    static constexpr index_type kRootIndex = 0UL;
+
+    constexpr index_type GetRootIndex() const { return kRootIndex; }
+    constexpr index_type GetNodeIndex() const { return (GetSize() - 1UL); }
+    constexpr index_type GetLeftNodeIndex(const index_type index) const { return (index * 2UL) + 1UL; }
+    constexpr index_type GetRightNodeIndex(const index_type index) const { return (index * 2UL) + 2UL; }
+    constexpr index_type GetParentNodeIndex(const index_type index) const { return ((index - 1UL) / 2UL); }
+
+    constexpr bool IsEmpty() const { return (0UL == GetSize()); }
+    constexpr bool IsFull() const { return (GetSize() >= GetCapacity()); }
+
+    constexpr bool IsIndexValid(const index_type index) const { return (index < GetSize()); }
+
+    constexpr bool HasLeftNode(const index_type index) const { return (GetLeftNodeIndex(index) < GetSize()); }
+    constexpr bool HasRightNode(const index_type index) const { return (GetRightNodeIndex(index) < GetSize()); }
+
+    constexpr value_type GetInvalidValue() const { return invalid_value_; }
+
+    constexpr value_type GetNodeValue(const index_type index) const { return buffer_.at(index); }
+    constexpr value_type GetLeftNodeValue(const index_type index) const { return GetNodeValue(GetLeftNodeIndex()); }
+    constexpr value_type GetRightNodeValue(const index_type index) const { return GetNodeValue(GetRightNodeIndex()); }
+    constexpr index_type GetParentNodeValue(const index_type index) const { return GetNodeValue(GetParentNodeIndex()); }
+
+    constexpr void SetNodeValue(const index_type index, const value_type& value) { buffer_.at(index) = value; }
+    constexpr void SetLeftNodeValue(const index_type index, const value_type& value)
+    {
+        SetNodeValue(GetLeftNodeIndex(index), value);
+    }
+    constexpr void SetRightNodeValue(const index_type index, const value_type& value)
+    {
+        SetNodeValue(GetRightNodeIndex(index), value);
+    }
+
+    constexpr index_type InsertBST(const index_type index, const value_type value)
+    {
+        index_type current_index = index;
+        if (!IsIndexValid(current_index))
+        {
+            SetNodeValue(current_index, value);
+            size_++;
+            return current_index;
+        }
+
+        if (value > GetNodeValue(current_index))
+        {
+            current_index = InsertBST(GetRightNodeIndex(current_index), value);
+            SetRightNodeValue(current_index, value);
+            size_++;
+        }
+        else
+        {
+            current_index = InsertBST(GetLeftNodeIndex(current_index), value);
+            SetLeftNodeValue(current_index, value);
+            size_++;
+        }
+        return current_index;
+    }
+
+    constexpr index_type InsertBT(const index_type index, const value_type value)
+    {
+        /// @todo: Add algorithm for inserting to Binary Tree..
+        return index_type{};
+    }
+
+    constexpr container InOrder(const index_type index) const { return container{}; }
+    constexpr container PostOrder(const index_type index) const { return container{}; }
+    constexpr container PreOrder(const index_type index) const { return container{}; }
+    constexpr size_type MaxDepth(const index_type index) const
+    {
+        size_type max_depth = 0UL;
+        if (IsIndexValid(index))
+        {
+            const size_type left_depth = MaxDepth(GetLeftNodeIndex(index));
+            const size_type right_depth = MaxDepth(GetRightNodeIndex(index));
+
+            max_depth = std::max(left_depth + 1UL, right_depth + 1UL);
+        }
+        return max_depth;
+    }
+    constexpr value_type MinValue(const index_type index) const
+    {
+        index_type current_index = index;
+        while (HasLeftNode(current_index))
+        {
+            current_index = GetLeftNodeIndex(current_index);
+        }
+        return GetNodeValue(current_index);
+    }
+    constexpr value_type MaxValue(const index_type index) const
+    {
+        index_type current_index = index;
+        while (HasRightNode(current_index))
+        {
+            current_index = GetRightNodeIndex(current_index);
+        }
+        return GetNodeValue(current_index);
+    }
+
+    constexpr bool IsBinarySearchTree(const index_type index) const
+    {
+        bool is_binary_search_tree = true;
+        if (IsIndexValid(index))
+        {
+            if (GetLeftNodeIndex(index) && (MaxValue(GetLeftNodeIndex(index)) > GetNodeValue(index)))
+            {
+                is_binary_search_tree = false;
+            }
+            else if (GetRightNodeIndex(index) && (MinValue(GetRightNodeIndex(index)) <= GetNodeValue(index)))
+            {
+                is_binary_search_tree = false;
+            }
+            else if ((!IsBinarySearchTree(GetLeftNodeIndex(index))) || (!IsBinarySearchTree(GetRightNodeIndex(index))))
+            {
+                is_binary_search_tree = false;
+            }
+            else
+            {
+                // do nothing
+            }
+        }
+        return is_binary_search_tree;
+    }
+
+    const value_type invalid_value_;
+    size_type size_;
+    container buffer_;
+};
+
+}  // namespace tree
+
+#endif  /// ALGORITHMS_TREE_BINARY_TREE_H
---
+++ algorithms/tree/test/binary_tree_tests.cpp
@@ -0,0 +1,45 @@
+///
+/// @file
+/// @copyright Copyright (C) 2021. MIT License.
+///
+#include "algorithms/tree/binary_tree.h"
+
+#include <gtest/gtest.h>
+
+#include <limits>
+
+namespace tree
+{
+namespace
+{
+static constexpr std::size_t kMaxNumberOfNodes = 10UL;
+
+class TreeFixture : public ::testing::Test
+{
+};
+
+template <typename T>
+class TreeFixtureT : public TreeFixture, public ::testing::WithParamInterface<T>
+{
+};
+
+TEST_F(TreeFixture, Insert_GivenTypicalNodes_ExpectUpdatedTree)
+{
+    // Given
+    Tree<std::int32_t, kMaxNumberOfNodes> unit{};
+
+    // When
+    unit.Insert(1);
+    unit.Insert(2);
+    unit.Insert(3);
+
+    // Then
+    EXPECT_EQ(unit.GetSize(), 3UL);
+    EXPECT_EQ(unit.GetCapacity(), kMaxNumberOfNodes);
+    EXPECT_EQ(unit.GetMaxDepth(), 1UL);
+    EXPECT_EQ(unit.GetMaxValue(), 3);
+    EXPECT_EQ(unit.GetMinValue(), 1);
+    EXPECT_TRUE(unit.IsBinarySearchTree());
+}
+}  // namespace
+}  // namespace tree
