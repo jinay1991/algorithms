413f964c8c398f9c4b001d4f93048d20e93a6efb
Jinay Patel <jinay1991@gmail.com>
2021-07-29 07:41:53 +0000

    Optimise space complexity

--- problem_statement/anagram.cpp
+++ problem_statement/anagram.cpp
@@ -17,22 +17,25 @@ bool IsAnagram(const std::string& s1, const std::string& s2)
     bool is_anagram = false;
     if (s1.size() == s2.size())
     {
-        std::array<std::int32_t, kMaxNumberOfCharacter> lookup1{};
-        std::array<std::int32_t, kMaxNumberOfCharacter> lookup2{};
+        std::array<std::int32_t, kMaxNumberOfCharacter> lookup{};
 
-        for (const auto& ch : s1)
+        for (auto idx = 0UL; idx < s1.size(); ++idx)
         {
-            const std::size_t position = static_cast<std::size_t>(ch);
-            lookup1.at(position) += 1;
+            const auto position1 = static_cast<std::size_t>(s1.at(idx));
+            const auto position2 = static_cast<std::size_t>(s2.at(idx));
+            lookup.at(position1)++;
+            lookup.at(position2)--;
         }
 
-        for (const auto& ch : s2)
+        is_anagram = true;
+        for (const auto& count : lookup)
         {
-            const std::size_t position = static_cast<std::size_t>(ch);
-            lookup2.at(position) += 1;
+            if (count != 0)
+            {
+                is_anagram = false;
+                break;
+            }
         }
-
-        is_anagram = (lookup1 == lookup2);
     }
     return is_anagram;
 }
